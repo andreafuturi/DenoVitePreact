5f5cac457fbd4256c2567b2739ae1ea69ac019f2
Andrea Futuri <andreascanu@icloud.com>
2024-11-20 13:46:09 +0200

    WIP-20-11-2024-13-46-09}

--- server/main.jsx
+++ server/main.jsx
@@ -2,30 +2,25 @@ import { refresh } from "https://deno.land/x/refresh/mod.ts";
 import { parse } from "https://deno.land/std/flags/mod.ts";
 import { render } from "https://esm.sh/preact-render-to-string?deps=preact";
 import App from "../client/index.jsx";
-import { createRouter } from "../lib/serverside-router.js";
 import { createServerHandler } from "../lib/server-handler.js";
 
 // Parse CLI args
 const args = parse(Deno.args);
 globalThis.dev = args.dev;
 
-// Setup configuration -> main app index jsx file, dev mode, static files directory,  middleware for dev auto refreshing
-const config = {
-  App,
-  isDev: globalThis.dev,
-  middleware: globalThis.dev ? refresh() : null,
-  staticDir: "client/",
+// Setup configuration -> main app index jsx, dev mode, static files directory,  middleware for dev auto refreshing
+const serverConfig = {
+  RootComponent: App,
+  renderFunction: render,
+  staticAssetsDirectory: "client/",
+  devMiddleware: globalThis.dev ? refresh() : null,
+  routingConfig: {
+    apiEndpointsPath: new URL(".", import.meta.url).pathname + "api",
+    pagesDirectory: new URL(".", import.meta.url).pathname + "../client",
+    isDevelopmentMode: globalThis.dev,
+  },
 };
 
-// Optionally setup router
-config.router = createRouter({
-  apiBasePath: new URL(".", import.meta.url).pathname + "api",
-  pageBasePath: new URL(".", import.meta.url).pathname + "../client",
-  render,
-  App,
-  isDev: globalThis.dev,
-});
-
 // Create and start server
-const handler = createServerHandler(config);
+const handler = createServerHandler(serverConfig);
 Deno.serve(handler);
